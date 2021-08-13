ccc3a7dfecf3343f16f366d2cdf94f50e218cc10
ToroChu <whileonrails@gmail.com>
2021-08-14 00:11:12 +0800

    add theme color

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,68 @@
 import React from "react";
 import "./App.css";
 
-function SwitchGroup({ children }) {
+function clsx(...args) {
+  return args.join(" ")
+}
+
+function SwitchGroup({ children, className, onChangeTheme }) {
+  function onChange(event) {
+    onChangeTheme(event.target.value)
+  }
+
   return (
-    <div>
-      <form className="w-16 h-7 border border-white">
-        <fieldset className="flex">{children}</fieldset>
-      </form>
-      <div className="w-16 h-7 border rounded-full flex justify-center items-center px-2">
-        <div className="bg-blue-500 rounded-full w-4 h-4"></div>
+    <div className={clsx("flex flex-col", className)}>
+      <div className="flex justify-between px-2">
+        {
+          [...Array(children.length)].map((i, index) => (<span className="w-7 text-center" key={index}>{index + 1}</span>))
+        }
       </div>
+      <form className="w-full border border-white rounded-full p-2" onChangeCapture={onChange}>
+        <fieldset className="flex justify-between">{children}</fieldset>
+      </form>
     </div>
   );
 }
 
-function SwitchBtn({ id, name, label }) {
+function SwitchBtn({ id, name, checked }) {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input type="radio" id={id} name={name} />
+    <div className="flex justify-center">
+      <input type="radio" id={id} name={name} value={id} className="sr-only peer" defaultChecked={checked} />
+      <label htmlFor={id} className="w-7 h-7 rounded-full text-center peer-checked:bg-blue-400"></label>
     </div>
   );
 }
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      theme: 'theme-two'
+    }
+  }
+
+  componentDidMount() {
+    const app = window.document.documentElement.querySelector("#App");
+    app.classList.add(this.state.theme)
+  }
+
+  onChangeTheme = (theme) => {
+    const app = window.document.documentElement.querySelector("#App");
+    console.log('before remove: ', this.state.theme)
+    app.classList.toggle(this.state.theme)
+    app.classList.add(theme)
+    this.setState({theme})
   }
 
   render() {
     return (
-      <div className="h-screen w-full bg-red-400">
-        <SwitchGroup label="Theme Switch">
-          <SwitchBtn id="theme-one" name="theme-switch" label="1" />
-          <SwitchBtn id="theme-two" name="theme-switch" label="2" />
-          <SwitchBtn id="theme-three" name="theme-switch" label="3" />
-        </SwitchGroup>
+      <div id="App" className="">
+        <div>
+          <SwitchGroup label="Theme Switch" className="w-40" onChangeTheme={this.onChangeTheme}>
+            <SwitchBtn id="theme-one" name="theme-switch" label="1" checked />
+            <SwitchBtn id="theme-two" name="theme-switch" label="2" />
+            <SwitchBtn id="theme-three" name="theme-switch" label="3" />
+          </SwitchGroup>
+        </div>
       </div>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          "mainBg": "hsl(222, 26%, 31%)",
+          "toggle": "hsl(223, 31%, 20%)",
+          "key": "hsl(223, 31%, 20%)",
+        },
+        light: {
+          "mainBg": "hsl(0, 0%, 90%)",
+          "toggle": "hsl(0, 5%, 81%)",
+          "key": "hsl(0, 5%, 81%)",
+        },
+        violet: {
+          "mainBg": "hsl(268, 75%, 9%)",
+          "toggle": "hsl(268, 71%, 12%)",
+          "key": "hsl(268, 71%, 12%)",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
