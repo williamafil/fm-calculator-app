8343115b093714275f276308419b5b23eb7cd9cd
ToroChu <whileonrails@gmail.com>
2021-08-14 13:49:40 +0800

    calc layout wip

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,27 @@ function SwitchGroup({ children, className, onChangeTheme }) {
     <div className={clsx("flex flex-col", className)}>
       <div className="flex justify-between px-2">
         {
-          [...Array(children.length)].map((i, index) => (<span className="w-7 text-center" key={index}>{index + 1}</span>))
+          [...Array(children.length)].map((i, index) => (<span className="w-4 text-xs font-bold text-center" key={index}>{index + 1}</span>))
         }
       </div>
-      <form className="w-full border border-white rounded-full p-2" onChangeCapture={onChange}>
+      <form className="switch-bg w-full rounded-full p-2" onChangeCapture={onChange}>
         <fieldset className="flex justify-between">{children}</fieldset>
       </form>
     </div>
   );
 }
 
-function SwitchBtn({ id, name, checked }) {
+function SwitchBtn({ id, name, currentTheme, checked }) {
   return (
     <div className="flex justify-center">
       <input type="radio" id={id} name={name} value={id} className="sr-only peer" defaultChecked={checked} />
-      <label htmlFor={id} className="w-7 h-7 rounded-full text-center peer-checked:bg-blue-400"></label>
+      <label htmlFor={id} className={
+        clsx(
+          "switch-btn w-4 h-4 rounded-full text-center ", 
+          currentTheme === 'theme-three' ? 'peer-checked:bg-cyan-toggle-btn' : 'peer-checked:bg-red-toggle-btn' 
+        )}>
+
+      </label>
     </div>
   );
 }
@@ -47,7 +53,6 @@ class App extends React.Component {
 
   onChangeTheme = (theme) => {
     const app = window.document.documentElement.querySelector("#App");
-    console.log('before remove: ', this.state.theme)
     app.classList.toggle(this.state.theme)
     app.classList.add(theme)
     this.setState({theme})
@@ -55,13 +60,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div id="App" className="">
-        <div>
-          <SwitchGroup label="Theme Switch" className="w-40" onChangeTheme={this.onChangeTheme}>
-            <SwitchBtn id="theme-one" name="theme-switch" label="1" checked />
-            <SwitchBtn id="theme-two" name="theme-switch" label="2" />
-            <SwitchBtn id="theme-three" name="theme-switch" label="3" />
-          </SwitchGroup>
+      <div id="App">
+        <div className="container mx-auto p-6">
+        <header className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold">calc</h1>
+          <div className="flex items-end">
+            <h2 className="uppercase text-xs font-bold mb-1 mr-5">Theme</h2>
+            <SwitchGroup label="Theme Switch" className="w-20" onChangeTheme={this.onChangeTheme}>
+              <SwitchBtn id="theme-one" name="theme-switch" label="1" currentTheme={this.state.theme} checked />
+              <SwitchBtn id="theme-two" name="theme-switch" label="2" currentTheme={this.state.theme} />
+              <SwitchBtn id="theme-three" name="theme-switch" label="3" currentTheme={this.state.theme} />
+            </SwitchGroup>
+          </div>
+        </header>
+        <main>
+          <section className="result mt-8 h-20 pr-6 flex items-center justify-end">
+            <h2 className="text-3xl font-bold">399,981</h2>
+          </section>
+          <section className="keypad">
+            <form className="">
+              <input type="button" value="1" />
+            </form>
+          </section>
+        </main>
         </div>
       </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,42 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        cyan: {
+          "toggle-btn": 'hsl(176, 100%, 44%)',
+        },
+        red: {
+          "toggle-btn": '#D03F2F',
+        },
         dark: {
           "mainBg": "hsl(222, 26%, 31%)",
+          "switchBg": "#242D44",
           "toggle": "hsl(223, 31%, 20%)",
           "key": "hsl(223, 31%, 20%)",
+          "text": "#FFFFFF",
+          "resultBg": "#181F33",
+          "keypadBg": "#242D44",
         },
         light: {
           "mainBg": "hsl(0, 0%, 90%)",
+          "switchBg": "#D2CDCD",
           "toggle": "hsl(0, 5%, 81%)",
           "key": "hsl(0, 5%, 81%)",
+          "text": "#36362C",
+          "resultBg": "#EEEEEE",
+          "keypadBg": "#D2CDCD",
         },
         violet: {
           "mainBg": "hsl(268, 75%, 9%)",
+          "switchBg": "#1E0936",
           "toggle": "hsl(268, 71%, 12%)",
           "key": "hsl(268, 71%, 12%)",
+          "text": "hsl(52, 100%, 62%)",
+          "resultBg": "#1E0936",
+          "keypadBg": "#1E0936",
         }
+      }, 
+      fontFamily: {
+        "spartan": ['Spartan', 'sans-serif']
       }
     },
   },
